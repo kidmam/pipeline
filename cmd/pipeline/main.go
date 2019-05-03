0d8ea805866497e7ead8fa5156715b8e7d15afd0
Zsolt Varga <zsolt.varga@console.hu>
2019-05-03 15:28:30 +0200

    Refactor cluster group API

--- cmd/pipeline/main.go
+++ cmd/pipeline/main.go
@@ -263,9 +263,6 @@ func main() {
 	clusterAPI := api.NewClusterAPI(clusterManager, clusterGetter, workflowClient, log, errorHandler, externalBaseURL, clusterCreators, clusterDeleters)
 
 	clusterGroupManager := clustergroup.NewManager(clusterManager, db, log, errorHandler)
-	clusterGroupDeploymentManager := clustergroup.NewCGDeploymentManager(db, log, errorHandler)
-	clusterGroupApi := api.NewClusterGroupAPI(clusterGroupManager, clusterGroupDeploymentManager, log, errorHandler)
-
 	federationHandler := clustergroup.NewFederationHandler(logger, errorHandler)
 	clusterGroupManager.RegisterFeatureHandler(clustergroup.FederationFeatureName, federationHandler)
 
